773faf6c36e8c25617c08584bc5c5cc9ac6f724b
Tomáš Valenta <semanticer@gmail.com>
2020-04-30 12:45:30 +0200

    Implement swipe actions

---
+++ timer/src/main/java/com/toggl/timer/log/ui/SwipeCallback.kt
@@ -0,0 +1,108 @@
+package com.toggl.timer.log.ui
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.drawable.ColorDrawable
+import android.text.Layout
+import android.text.StaticLayout
+import android.text.TextPaint
+import android.util.Log
+import android.view.View
+import androidx.annotation.ColorInt
+import androidx.core.graphics.withTranslation
+import androidx.recyclerview.widget.ItemTouchHelper
+import com.airbnb.epoxy.EpoxyModel
+import com.airbnb.epoxy.EpoxyTouchHelper
+import com.toggl.models.common.SwipeDirection
+
+class SwipeCallback(
+    private val swipeLeftParams: SwipeActionParams,
+    private val swipeRightParams: SwipeActionParams,
+    private val onSwipeActionListener: (model: EpoxyModel<Any>, SwipeDirection) -> Unit
+) : EpoxyTouchHelper.SwipeCallbacks<EpoxyModel<Any>>() {
+
+    private val swipeBackground = ColorDrawable(swipeLeftParams.backgroundColor)
+    private val swipeLeftLabelLayout = swipeLeftParams.buildLabel()
+    private val swipeRightLabelLayout = swipeRightParams.buildLabel()
+
+    override fun onSwipeCompleted(model: EpoxyModel<Any>, itemView: View, position: Int, direction: Int) {
+        onSwipeActionListener(
+            model,
+            if (direction == ItemTouchHelper.LEFT) SwipeDirection.Left
+            else SwipeDirection.Right
+        )
+    }
+
+    override fun onSwipeProgressChanged(
+        model: EpoxyModel<Any>,
+        itemView: View,
+        swipeProgress: Float,
+        c: Canvas
+    ) {
+        val dX = (itemView.width * swipeProgress).toInt()
+        val swipingToTheRight = swipeProgress > 0
+        val swipingToTheLeft = swipeProgress < 0
+
+        with(swipeBackground) {
+            when {
+                swipingToTheRight -> {
+                    color = swipeRightParams.backgroundColor
+                    setBounds(
+                        itemView.left,
+                        itemView.top,
+                        itemView.left + dX,
+                        itemView.bottom
+                    )
+                }
+                swipingToTheLeft -> {
+                    color = swipeLeftParams.backgroundColor
+                    swipeBackground.setBounds(
+                        itemView.right + dX,
+                        itemView.top,
+                        itemView.right,
+                        itemView.bottom
+                    )
+                }
+                else -> setBounds(0, 0, 0, 0)
+            }
+            draw(c)
+        }
+
+        val labelYPos = itemView.top.toFloat() + (itemView.height / 2) - (swipeRightLabelLayout.height / 2)
+        when {
+            swipingToTheRight -> c.withTranslation(itemView.left.toFloat() + swipeRightParams.textPadding, labelYPos) {
+                swipeRightLabelLayout.draw(c)
+            }
+            swipingToTheLeft -> c.withTranslation(itemView.right.toFloat() - swipeLeftLabelLayout.width - swipeLeftParams.textPadding, labelYPos) {
+                swipeLeftLabelLayout.draw(c)
+            }
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun SwipeActionParams.buildLabel(): StaticLayout {
+        val textPaint = TextPaint().apply {
+            color = textColor
+            textSize = fontSize
+            isAntiAlias = true
+        }
+        return StaticLayout(
+            text,
+            textPaint,
+            textPaint.measureText(text).toInt(),
+            Layout.Alignment.ALIGN_NORMAL,
+            1f,
+            0f,
+            false
+        )
+    }
+}
+
+data class SwipeActionParams(
+    val text: String,
+    @ColorInt val backgroundColor: Int,
+    @ColorInt val textColor: Int,
+    val fontSize: Float,
+    val textPadding: Float
+)
--- timer/src/main/java/com/toggl/timer/log/ui/TimeEntriesLogFragment.kt
+++ timer/src/main/java/com/toggl/timer/log/ui/TimeEntriesLogFragment.kt
@@ -1,18 +1,24 @@
 package com.toggl.timer.log.ui
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.airbnb.epoxy.EpoxyModel
+import com.airbnb.epoxy.EpoxyTouchHelper
 import com.google.android.material.snackbar.Snackbar
 import com.toggl.common.Constants.timeEntryDeletionDelayMs
 import com.toggl.common.deepLinks
 import com.toggl.common.performClickHapticFeedback
 import com.toggl.environment.services.time.TimeService
+import com.toggl.models.common.SwipeDirection
 import com.toggl.timer.R
 import com.toggl.timer.di.TimerComponentProvider
 import com.toggl.timer.log.domain.DayHeaderViewModel
@@ -28,7 +34,7 @@ import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
+import java.lang.IllegalStateException
 import java.util.Locale
 import javax.inject.Inject
 
@@ -43,6 +49,20 @@ class TimeEntriesLogFragment : Fragment(R.layout.fragment_time_entries_log) {
 
     private var snackbar: Snackbar? = null
 
+    private val curriedTimeEntriesSelector: suspend (TimeEntriesLogState) -> List<TimeEntryViewModel> = {
+        timeEntriesLogSelector(
+            it.timeEntries,
+            it.projects,
+            it.clients,
+            timeService,
+            getString(R.string.today),
+            getString(R.string.yesterday),
+            true,
+            it.expandedGroupIds,
+            it.entriesPendingDeletion
+        )
+    }
+
     override fun onAttach(context: Context) {
         (requireActivity().applicationContext as TimerComponentProvider)
             .provideTimerComponent().inject(this)
@@ -53,49 +73,33 @@ class TimeEntriesLogFragment : Fragment(R.layout.fragment_time_entries_log) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val context = requireContext()
+        store.state
+            .map(curriedTimeEntriesSelector)
+            .distinctUntilChanged()
+            .onEach(::updateList)
+            .launchIn(lifecycleScope)
 
-        val todayString = context.getString(R.string.today)
-        val yesterdayString = context.getString(R.string.yesterday)
-
-        val curriedTimeEntriesSelector: suspend (TimeEntriesLogState) -> List<TimeEntryViewModel> = {
-            timeEntriesLogSelector(
-                it.timeEntries,
-                it.projects,
-                it.clients,
-                timeService,
-                todayString,
-                yesterdayString,
-                true,
-                it.expandedGroupIds,
-                it.entriesPendingDeletion
-            )
-        }
-
-        lifecycleScope.launch {
-            store.state
-                .map(curriedTimeEntriesSelector)
-                .distinctUntilChanged()
-                .onEach(::updateList)
-                .launchIn(this)
-
-            store.state
-                .map { it.editableTimeEntry != null }
-                .distinctUntilChanged()
-                .onEach { isEditViewExpanded ->
-                    if (isEditViewExpanded) {
-                        this@TimeEntriesLogFragment.context?.performClickHapticFeedback()
-                        findNavController().navigate(deepLinks.timeEntriesStartEditDialog)
-                    }
+        store.state
+            .map { it.editableTimeEntry != null }
+            .distinctUntilChanged()
+            .onEach { isEditViewExpanded ->
+                if (isEditViewExpanded) {
+                    this@TimeEntriesLogFragment.context?.performClickHapticFeedback()
+                    findNavController().navigate(deepLinks.timeEntriesStartEditDialog)
                 }
-                .launchIn(this)
+            }
+            .launchIn(lifecycleScope)
 
-            store.state
-                .map { it.entriesPendingDeletion }
-                .distinctUntilChanged()
-                .onEach { showUndoDeletionSnackbar(it) }
-                .launchIn(this)
-        }
+        store.state
+            .map { it.entriesPendingDeletion }
+            .distinctUntilChanged()
+            .onEach { showUndoDeletionSnackbar(it) }
+            .launchIn(lifecycleScope)
+
+        EpoxyTouchHelper.initSwiping(recycler_view)
+            .leftAndRight()
+            .withTargets(TimeEntryItemModel_::class.java, TimeEntryGroupModel_::class.java)
+            .andCallbacks(createSwipeActionCallback(requireContext()))
     }
 
     private suspend fun updateList(items: List<TimeEntryViewModel>) {
@@ -123,6 +127,48 @@ class TimeEntriesLogFragment : Fragment(R.layout.fragment_time_entries_log) {
                 }
             }
         }
+    }
+
+    private fun onLogItemSwiped(model: EpoxyModel<Any>, swipeDirection: SwipeDirection) {
+        when (model) {
+            is TimeEntryItemModel_ -> TimeEntriesLogAction.TimeEntrySwiped(model.timeEntry.id, swipeDirection)
+            is TimeEntryGroupModel_ -> TimeEntriesLogAction.TimeEntryGroupSwiped(model.timeEntryGroup.timeEntryIds, swipeDirection)
+            else -> throw IllegalStateException("Swiping unsupported model $model")
+        }.let(store::dispatch)
+
+        if (swipeDirection == SwipeDirection.Right) {
+            // recycler_view.adapter?.notifyItemChanged(position) we need this to revert continue animation but it's it is forbidden to call this in epoxy
+            recycler_view.requestModelBuild()
+        }
+    }
+
+    private fun createSwipeActionCallback(context: Context): SwipeCallback {
+        val leftSwipeColor = ContextCompat.getColor(context, R.color.stop_time_entry_button_background)
+        val rightSwipeColor = ContextCompat.getColor(context, R.color.start_time_entry_button_background)
+        val actionTextColor = ContextCompat.getColor(context, R.color.text_on_surface)
+        val actionFontSize = resources.getDimension(R.dimen.swipe_action_font_size)
+        val actionPadding = resources.getDimension(R.dimen.swipe_action_text_padding)
+
+        val swipeLeftParams = SwipeActionParams(
+            text = getString(R.string.delete),
+            backgroundColor = leftSwipeColor,
+            textColor = actionTextColor,
+            fontSize = actionFontSize,
+            textPadding = actionPadding
+        )
+        val swipeRightParams = SwipeActionParams(
+            text = getString(R.string.continue_this),
+            backgroundColor = rightSwipeColor,
+            textColor = actionTextColor,
+            fontSize = actionFontSize,
+            textPadding = actionPadding
+        )
+
+        return SwipeCallback(
+            swipeLeftParams = swipeLeftParams,
+            swipeRightParams = swipeRightParams,
+            onSwipeActionListener = ::onLogItemSwiped
+        )
     }
 
     override fun onDestroyView() {
