85e14524bda3e0c6d00ba1e545f9d98b3ecd1a1c
Simon Rozsival <simon@rozsival.com>
2020-07-23 13:12:30 +0200

    :white_check_mark: Add unit test for the reducer

--- settings/src/test/java/com/toggl/settings/domain/SettingTappedActionTests.kt
+++ settings/src/test/java/com/toggl/settings/domain/SettingTappedActionTests.kt
@@ -1,11 +1,15 @@
 package com.toggl.settings.domain
 
+import com.toggl.common.feature.navigation.Route
 import com.toggl.models.domain.SettingsType
 import com.toggl.settings.common.CoroutineTest
 import com.toggl.settings.common.createSettingsReducer
 import com.toggl.settings.common.createSettingsState
 import com.toggl.settings.common.testReduceEffects
+import com.toggl.settings.common.testReduceState
 import io.kotlintest.matchers.collections.shouldNotBeEmpty
+import io.kotlintest.matchers.numerics.shouldBeGreaterThan
+import io.kotlintest.shouldBe
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.jupiter.api.DisplayName
@@ -23,5 +27,16 @@ class SettingTappedActionTests : CoroutineTest() {
             initialState,
             SettingsAction.SettingTapped(SettingsType.ManualMode)
         ) { effect -> effect.shouldNotBeEmpty() }
+    }
+
+    @Test
+    fun `The About setting should navigate to the About view`() = runBlockingTest {
+        reducer.testReduceState(
+            initialState,
+            SettingsAction.SettingTapped(SettingsType.About)
+        ) { state ->
+            state.backStack.size shouldBeGreaterThan initialState.backStack.size
+            state.backStack.last() shouldBe Route.SettingsEdit(SettingsType.About)
+        }
     }
 }
