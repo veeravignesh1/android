d75c6b94276cbfcaf79fcfe93011628ede5977b4
peter <53510080+pdrj@users.noreply.github.com>
2020-07-29 10:09:53 +0200

    Reuse settings deeplink for single choice dialogs

--- common-feature/src/main/java/com/toggl/common/feature/navigation/Route.kt
+++ common-feature/src/main/java/com/toggl/common/feature/navigation/Route.kt
@@ -56,7 +56,7 @@ fun Route.deepLink(deepLinks: DeepLinkUrls): Uri {
         is Route.StartEdit -> deepLinks.startEditDialog
         is Route.Project -> deepLinks.projectDialog
         is Route.ContextualMenu -> deepLinks.contextualMenu
-        is Route.SettingsDialog -> TODO()
+        is Route.SettingsDialog -> deepLinks.settings
         Route.Settings -> deepLinks.settings
         Route.CalendarSettings -> deepLinks.calendarSettings
         Route.Feedback -> deepLinks.submitFeedback
