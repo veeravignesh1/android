67121d1a9b6d8a2976000bdb0ff65f71ef1e0d5d
peter <53510080+pdrj@users.noreply.github.com>
2020-07-30 12:26:52 +0200

    Remove embeded single choice dialog from settings

--- settings/src/main/java/com/toggl/settings/ui/SettingsFragment.kt
+++ settings/src/main/java/com/toggl/settings/ui/SettingsFragment.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.Recomposer
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.ui.core.setContent
 import com.toggl.architecture.extensions.select
 import com.toggl.settings.compose.extensions.createComposeView
 import com.toggl.settings.domain.SettingsSelector
-import com.toggl.settings.domain.SingleChoiceSettingSelector
-import com.toggl.settings.ui.composables.SingleChoiceDialogWithHeader
 import com.toggl.settings.ui.composables.pages.SettingsPage
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -19,8 +18,9 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment() {
-    @Inject @JvmField var settingsSelector: SettingsSelector? = null // https://github.com/google/dagger/issues/1883#issuecomment-642565920 🤷‍
-    @Inject @JvmField var singleChoiceSettingSelector: SingleChoiceSettingSelector? = null // https://github.com/google/dagger/issues/1883#issuecomment-642565920 🤷‍
+    @Inject
+    @JvmField
+    var settingsSelector: SettingsSelector? = null // https://github.com/google/dagger/issues/1883#issuecomment-642565920 🤷‍
     private val store: SettingsStoreViewModel by viewModels()
 
     @ExperimentalCoroutinesApi
@@ -29,9 +29,8 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = createComposeView { statusBarHeight, navigationBarHeight ->
-        setContent(androidx.compose.Recomposer.current()) {
+        setContent(Recomposer.current()) {
             val selectedState = store.state.select(settingsSelector!!)
-            val selectedSingleChoiceState = store.state.select(singleChoiceSettingSelector!!)
 
             SettingsPage(
                 selectedState,
@@ -39,8 +38,6 @@ class SettingsFragment : Fragment() {
                 navigationBarHeight,
                 store::dispatch
             )
-
-            SingleChoiceDialogWithHeader(selectedSingleChoiceState, store::dispatch)
         }
     }
 }
