b8770548104230ce22eeab085de027c4ef99d839
peter <53510080+pdrj@users.noreply.github.com>
2020-07-30 12:28:24 +0200

    Add single choice dialog fragment

---
+++ settings/src/main/java/com/toggl/settings/ui/SingleChoiceSettingsDialogFragment.kt
@@ -0,0 +1,34 @@
+package com.toggl.settings.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.toggl.architecture.extensions.select
+import com.toggl.settings.compose.extensions.createComposeDialog
+import com.toggl.settings.domain.SingleChoiceSettingSelector
+import com.toggl.settings.ui.composables.SingleChoiceDialogWithHeader
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class SingleChoiceSettingsDialogFragment : DialogFragment() {
+    @Inject
+    @JvmField
+    var singleChoiceSettingSelector: SingleChoiceSettingSelector? = null // https://github.com/google/dagger/issues/1883#issuecomment-642565920 🤷‍
+    private val store: SettingsStoreViewModel by viewModels()
+
+    @ExperimentalCoroutinesApi
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = createComposeDialog {
+        val selectedSingleChoiceState = store.state.select(singleChoiceSettingSelector!!)
+
+        SingleChoiceDialogWithHeader(selectedSingleChoiceState, store::dispatch)
+    }
+}
