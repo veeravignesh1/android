f836412a72a91b3662136f66894973ea6bee9153
pdrj <53510080+pdrj@users.noreply.github.com>
2020-08-03 13:23:54 +0200

    🔒 Add strong password indicators (#699)
    * Add criteria message
    
    * Add strength indicators
    
    * Add strong passwords
    
    * Constructor not acessible
    
    * Make passwords regular classes
    
    * Spotless
    
    * Fix login
    
    * Remove needless cast
    
    * Password is already in the state
    
    * Fix helpers
    
    * Remove unused strings (results of a bad merge)

--- models/src/main/java/com/toggl/models/validation/Password.kt
+++ models/src/main/java/com/toggl/models/validation/Password.kt
@@ -3,13 +3,13 @@ package com.toggl.models.validation
 sealed class Password {
     abstract val password: String
 
-    data class Invalid internal constructor(override val password: String) : Password()
+    class Invalid internal constructor(override val password: String) : Password()
     open class Valid internal constructor(override val password: String) : Password() {
         override fun equals(other: Any?) = other is Valid && other.password == password
 
         override fun hashCode() = password.hashCode()
     }
-    data class Strong internal constructor(override val password: String) : Valid(password)
+    class Strong internal constructor(override val password: String) : Valid(password)
 
     override fun toString(): String = password
 
@@ -22,7 +22,7 @@ sealed class Password {
             }
 
         private fun String.isStrong() =
-            length > 8 && any { it.isDigit() } && any { it.isUpperCase() } && any { it.isLowerCase() }
+            length >= 8 && any { it.isDigit() } && any { it.isUpperCase() } && any { it.isLowerCase() }
     }
 }
 
--- onboarding/src/main/java/com/toggl/onboarding/signup/ui/SignUpFragment.kt
+++ onboarding/src/main/java/com/toggl/onboarding/signup/ui/SignUpFragment.kt
@@ -1,18 +1,22 @@
 package com.toggl.onboarding.signup.ui
 
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.toggl.common.extensions.doSafeAfterTextChanged
 import com.toggl.common.extensions.setSafeText
+import com.toggl.models.validation.Password
 import com.toggl.onboarding.R
 import com.toggl.onboarding.signup.domain.SignUpAction
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_signup.*
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
@@ -33,6 +37,20 @@ class SignUpFragment : Fragment(R.layout.fragment_signup) {
 
         login_button.setOnClickListener {
             store.dispatch(SignUpAction.GoToLoginTapped)
+        }
+
+        val defaultCriteriaColor = ContextCompat.getColor(requireContext(), R.color.authentication_text)
+        val errorCriteriaColor = ContextCompat.getColor(requireContext(), R.color.error)
+
+        store.state
+            .map { it.password is Password.Strong }
+            .filter { it }
+            .onEach { password_strength_criteria.setTextColor(defaultCriteriaColor) }
+            .launchIn(lifecycleScope)
+
+        sign_up_button.setOnClickListener {
+            if (!password.text.representsStrongPassword())
+                password_strength_criteria.setTextColor(errorCriteriaColor)
         }
     }
 
@@ -56,4 +74,7 @@ class SignUpFragment : Fragment(R.layout.fragment_signup) {
 
         super.onDestroyView()
     }
+
+    private fun Editable?.representsStrongPassword() =
+        if (this == null) false else Password.from(this.toString()) is Password.Strong
 }
